f696558902be4ac1b13575c9a54e826a49433b72
Wiktor N <osm@vink.pl>
2014-10-31 19:23:00 +0100

    remove debugging

--- rest-server.py
+++ rest-server.py
@@ -38,4 +38,4 @@ def fullImport(name):
     return make_response(ret, 200)
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5001, debug=True)
+    app.run(host='0.0.0.0', port=5001, debug=False)
