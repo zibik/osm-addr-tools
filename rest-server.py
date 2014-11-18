372f84ebc425a69c91d69303d7b688be7b932793
W <github@vink.pl>
2014-11-18 21:28:30 +0100

    added logging for rest server

--- rest-server.py
+++ rest-server.py
@@ -2,6 +2,7 @@ from flask import Flask, make_response as _make_response
 from merger import mergeInc, mergeFull, getAddresses
 from punktyadresowe_import import iMPA
 import utils
+import logging
 
 app = Flask(__name__)
 
@@ -33,4 +34,12 @@ def fullImport(name):
     return make_response(ret, 200)
 
 if __name__ == '__main__':
+    ADMINS = ['logi-osm@vink.pl']
+    if not app.debug:
+        from logging.handlers import SMTPHandler
+        mail_handler = SMTPHandler('127.0.0.1',
+                                   'server-error@vink.pl',
+                                   ADMINS, 'OSM Rest-Server Failed')
+        mail_handler.setLevel(logging.INFO)
+        app.logger.addHandler(mail_handler)
     app.run(host='0.0.0.0', port=5001, debug=False)
