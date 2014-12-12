af4edefac5669cd910a8f8e588698406f00f88d7
W <github@vink.pl>
2014-12-12 20:54:26 +0100

    added grouping of calls in separate loops, one per therad

--- utils.py
+++ utils.py
@@ -1,6 +1,7 @@
 from concurrent.futures import ThreadPoolExecutor
 import functools
 import multiprocessing
+import itertools
 
 __executor = ThreadPoolExecutor(max_workers=multiprocessing.cpu_count()) # pick sane default - number of CPUs
 
@@ -13,11 +14,15 @@ def parallel_execution(*args):
     )
 
 def parallel_map(func, lst):
-    return tuple(
-        map(lambda x: x.result(),
-            [__executor.submit(functools.partial(func, x)) for x in lst]
-        )
-    )
+    cpu = multiprocessing.cpu_count()
+    iters = (itertools.islice(lst, x, None, cpu) for x in range(cpu))
+    def loop(l):
+        return tuple(map(func, l))
+    
+    return tuple(itertools.chain.from_iterable(
+            zip(*parallel_execution(*tuple(functools.partial(loop, x) for x in iters)))
+   ))
+
 
 def groupby(lst, keyfunc=lambda x: x, valuefunc=lambda x: x):
     ret = {}
@@ -34,7 +39,12 @@ def groupby(lst, keyfunc=lambda x: x, valuefunc=lambda x: x):
 def main():
     from time import sleep,time
     print(time())
-    parallel_execution(lambda: sleep(5), lambda: sleep(5), lambda: sleep(5), lambda: sleep(5), lambda: sleep(5))
+    parallel_execution(lambda: sleep(1), lambda: sleep(1), lambda: sleep(1), lambda: sleep(1), lambda: sleep(1))
+    print(time())
+    def f(x):
+        sleep(1)
+        return str(x)
+    print(parallel_map(f, [1,2,3,4,5,6,7,8,9,10,11,12,13]))
     print(time())
 
 
